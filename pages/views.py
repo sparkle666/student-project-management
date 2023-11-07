0c095dce5b78ade57d7370b58f58fa62ff2c595a
sparkle666 <sikky606@gmail.com>
2023-11-07 22:52:37 +0100

    fix: error in the list project view, should list projects of current user

--- pages/views.py
+++ pages/views.py
@@ -7,7 +7,6 @@ from django.contrib import auth
 from django.urls import reverse
 
 
-
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
@@ -23,6 +22,7 @@ def create_project(request):
             # Save the project to the database
             project = form.save(commit=False)
             project.submitted_request = True  # You can set other fields as needed
+            project.student = request.user
             project.save()
             # Perform any other actions or redirection here
             return redirect('project_submitted', project_id=project.id)
@@ -39,8 +39,13 @@ def project_submitted(request, project_id):
 
 
 def projects_list(request):
-    projects = Project.objects.all()
-    return render(request, 'pages/project_list.html', {'projects': projects})
+    projects = Project.objects.filter(student=request.user)
+
+    context = {'projects': projects}
+    # if not projects.exists():
+    #     context = {'projects': []}
+
+    return render(request, 'pages/project_list.html', context)
 
   # Create a form for supervisor requests
 
@@ -83,11 +88,13 @@ def superuser_signup(request):
             user.save()
 
             # Log in the superuser
-            user = auth.authenticate(username=user.username, password=request.POST['password1'])
+            user = auth.authenticate(
+                username=user.username, password=request.POST['password1'])
             if user is not None:
                 auth.login(request, user)
 
-            return redirect(reverse('admin:index'))  # Redirect to the admin dashboard.
+            # Redirect to the admin dashboard.
+            return redirect(reverse('admin:index'))
     else:
         form = SuperuserCreationForm()
     return render(request, 'account/superuser_signup.html', {'form': form})
